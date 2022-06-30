f65564ef9d61d5a4deee3696587d09f1073e8145
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-06-30 09:45:51 -0500

    commit tree 49996df935c2f4c5d976250817c9e2a4463070ba

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,43 @@
-# your code here
+class MySet
+  def initialize(enumerable = [])
+    @hash = {}
+    enumerable.each {|v| @hash[v] = true}
+  end
+
+  def self.[](*args)
+    new(args)
+  end
+
+  def include?(v)
+    @hash[v] != nil
+    # @hash.has_key?(v)
+  end
+
+  def add(v)
+    @hash[v] = true
+    self
+  end
+
+  def delete(v)
+    @hash.delete v
+    self
+  end
+
+  def size
+    @hash.size
+  end
+
+  def clear
+    @hash = {}
+    self
+  end
+
+  def each
+    size.times {|v| v}
+    self
+  end
+
+  def inspect
+    @hash.keys
+  end
+end
